d1d79715f8ed722799cdb3ad6f30a320418875c6
zimi <92szymon@gmail.com>
2017-01-01 15:56:06 +0100

    scene added, first model added and test-scripted

---
+++ AlphaProhibitionGame/Assets/scripts/AIobject1Behaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIobject1Behaviour : MonoBehaviour {
+	//global variables
+	Rigidbody2D rigi;
+	float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+		rigi = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		keyboardMovement ();
+	}
+
+	private void keyboardMovement()
+	{
+		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		transform.position += move * speed * Time.deltaTime;
+	}
+}
